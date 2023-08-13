d8de5301a663f675fc190d79278a008ccbc36c02
DenysShchypt <denysshchyptsovit@gmail.com>
2023-08-13 22:03:11 +0300

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
 
+
+// Додав стилізацію на кнопки
+
+const elements_1 = {
+    btnStart: document.querySelector('button[data-start]'),
+    btnStop: document.querySelector('button[data-stop]'),
+    bodyBgColor: document.querySelector('body'),
+}
+
+let bgBodyColor = null;
+elements_1.btnStart.classList.add('btn-js');
+elements_1.btnStop.classList.add('btn-js');
+elements_1.btnStart.addEventListener('click', handlerStartBtn);
+elements_1.btnStop.addEventListener('click', handlerStopBtn);
+
+function handlerStartBtn() {
+    if (!bgBodyColor) {
+        bgBodyColor = setInterval(() => {
+            elements_1.bodyBgColor.style.backgroundColor = getRandomHexColor();
+        }, 1000)
+    }
+}
+function handlerStopBtn() {
+    clearInterval(bgBodyColor)
+    bgBodyColor = false;
+};
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
