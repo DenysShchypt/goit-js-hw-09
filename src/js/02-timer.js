f9343a0ea7b1700f213531e612e66e9eb349aef2
DenysShchypt <denysshchyptsovit@gmail.com>
2023-08-16 17:37:07 +0300

    Task3

---
+++ src/js/02-timer.js
@@ -0,0 +1,74 @@
+// Описаний в документації
+import flatpickr from "flatpickr";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// Додатковий імпорт стилів
+import "flatpickr/dist/flatpickr.min.css";
+const element_2 = {
+    inputDate: document.querySelector('input#datetime-picker'),
+    btnStart: document.querySelector('button[data-start]'),
+    valueDays: document.querySelector('.value[data-days]'),
+    valueHours: document.querySelector('.value[data-hours]'),
+    valueMinutes: document.querySelector('.value[data-minutes]'),
+    valueSeconds: document.querySelector('.value[data-seconds]'),
+};
+
+element_2.btnStart.addEventListener('click', handlerBtnStart);
+element_2.btnStart.disabled = true;
+const currentData = Date.now();
+
+let endDate = null;
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        endDate = Number(selectedDates[0]);
+        if (endDate < currentData) {
+            Notify.failure('Please choose a date in the future');
+        }
+        else {
+            element_2.btnStart.disabled = false;
+        }
+    },
+};
+
+flatpickr(element_2.inputDate, options);
+
+function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+
+};
+
+function convertMs(ms) {
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+    const days = addLeadingZero(Math.floor(ms / day));
+    const hours = addLeadingZero(Math.floor((ms % day) / hour));
+    const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+    const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+    return { days, hours, minutes, seconds };
+};
+
+function handlerBtnStart(e) {
+    const startTimer = setInterval(() => {
+        const startDate = Date.now();
+        const ms = endDate - startDate;
+        const remainingTime = convertMs(ms);
+        updateClockface(remainingTime)
+    }, 1000);
+    element_2.btnStart.disabled = true;
+};
+
+function updateClockface({ days, hours, minutes, seconds }) {
+
+    element_2.valueDays.textContent = `${days}`;
+    element_2.valueHours.textContent = `${hours}`;
+    element_2.valueMinutes.textContent = `${minutes}`;
+    element_2.valueSeconds.textContent = `${seconds}`;
+
+};
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,56 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  formBox: document.querySelector('.form'),
 }
+
+refs.formBox.classList.add('js-container');
+refs.formBox.addEventListener('submit', hadlerBtnPromis);
+
+function createPromise(position, delay) {
+
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+
+};
+
+function hadlerBtnPromis(e) {
+
+  e.preventDefault();
+  const { delay, step, amount } = e.currentTarget.elements;
+  const positionPromis = Number(amount.value) + 1;
+  const stepTimeValue = Number(step.value)
+  let stepTimePromis = Number(delay.value);
+  numberChallengePromises(stepTimePromis, stepTimeValue, positionPromis)
+
+};
+
+function numberChallengePromises(stepTimePromis, stepTimeValue, positionPromis) {
+
+  challengePromises(1, stepTimePromis);
+  for (let i = 2; i < positionPromis; i += 1) {
+    stepTimePromis += stepTimeValue;
+    challengePromises(i, stepTimePromis);
+
+  };
+};
+
+function challengePromises(numberPromis, time) {
+
+  createPromise(numberPromis, time)
+    .then(({ position, delay }) => {
+      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+     Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+
+};
